4ac5aedcbb29a47f65595bbf393651761bf10b95
Songmu <y.songmu@gmail.com>
2016-05-04 14:21:11 +0900

    test enhancment

--- gitsemvers_test.go
+++ gitsemvers_test.go
@@ -1,8 +1,11 @@
 package gitsemvers
 
 import (
+	"os"
 	"reflect"
 	"testing"
+
+	"github.com/Songmu/gitmock"
 )
 
 var input = `dummy
@@ -70,5 +73,75 @@ func TestParseVersionsWithAllExtensions(t *testing.T) {
 	sv := &Semvers{WithPreRelease: true, WithBuildMetadata: true}
 	if !reflect.DeepEqual(sv.parseVersions(input), expect) {
 		t.Errorf("something went wrong %+v", sv.parseVersions(input))
+	}
+}
+
+func TestVersionStrings(t *testing.T) {
+	gm, err := gitmock.New("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	repoPath := gm.RepoPath()
+	defer os.RemoveAll(repoPath)
+	gm.Init()
+	gm.Commit("--allow-empty", "-m", "initial commit")
+	gm.Tag("0.0.1")
+	gm.Tag("v0.0.2")
+	gm.Tag("v0.0.2-pre")
+	gm.Tag("v0.0.2-pre+win")
+	gm.Tag("v0.0.2+win")
+	gm.Tag("v0.0.2+win+invalid")
+
+	sv := &Semvers{
+		RepoPath: repoPath,
+	}
+	{
+		expect := []string{
+			"v0.0.2",
+			"0.0.1",
+		}
+		if !reflect.DeepEqual(sv.VersionStrings(), expect) {
+			t.Errorf("something went wrong")
+		}
+	}
+
+	sv.WithPreRelease = true
+	{
+		expect := []string{
+			"v0.0.2",
+			"v0.0.2-pre",
+			"0.0.1",
+		}
+		if !reflect.DeepEqual(sv.VersionStrings(), expect) {
+			t.Errorf("something went wrong %+v", sv.VersionStrings())
+		}
+	}
+
+	sv.WithPreRelease = false
+	sv.WithBuildMetadata = true
+	{
+		expect := []string{
+			"v0.0.2",
+			"v0.0.2+win",
+			"0.0.1",
+		}
+		if !reflect.DeepEqual(sv.VersionStrings(), expect) {
+			t.Errorf("something went wrong %+v", sv.VersionStrings())
+		}
+	}
+
+	sv.WithPreRelease = true
+	sv.WithBuildMetadata = true
+	{
+		expect := []string{
+			"v0.0.2",
+			"v0.0.2+win",
+			"v0.0.2-pre",
+			"v0.0.2-pre+win",
+			"0.0.1",
+		}
+		if !reflect.DeepEqual(sv.VersionStrings(), expect) {
+			t.Errorf("something went wrong %+v", sv.VersionStrings())
+		}
 	}
 }
